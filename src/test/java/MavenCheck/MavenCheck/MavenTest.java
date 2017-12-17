777bcd49139be8bd034a4e6cf431c4a888b56818
souvikchandra1984 <souvik.chandra@gmail.com>
2017-12-17 17:21:32 +0530

    s

--- src/test/java/MavenCheck/MavenCheck/MavenTest.java
+++ src/test/java/MavenCheck/MavenCheck/MavenTest.java
@@ -14,23 +14,32 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 import org.apache.commons.io.FileUtils;
 
-@Test (threadPoolSize = 3)
+
 public class MavenTest {
 
-	public void check() throws IOException{
-	System.setProperty("webdriver.gecko.driver", "D://Souvik//Selenium//Selenium Jar//geckodriver-v0.11.1-win64//geckodriver.exe");
+	@Test
+	public void check1() throws IOException{
+	//System.setProperty("webdriver.gecko.driver", "D://Souvik//Selenium//Selenium Jar//geckodriver-v0.11.1-win64//geckodriver.exe");
 	System.out.println("Hello");
-	Reporter.log("Hellp"); 
-	WebDriver driver = new FirefoxDriver();
+	Reporter.log("Inside 1st Methos"); 
+	Reporter.log("\n");
+	Reporter.log("3rd line");
+/*	WebDriver driver = new FirefoxDriver();
 	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 	//The below method will save the screen shot in d drive with name "screenshot.png"
 	String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 	File screenShotName = new File("D:\\MyTest\\Screenshots\\"+timeStamp+".png");
 	FileUtils.copyFile(scrFile, screenShotName);
-	Reporter.log(screenShotName.toString());
+	Reporter.log(screenShotName.toString());*/
 	Assert.assertEquals("2", "2");
 	//WebDriver d = new FirefoxDriver();
 	//d.get("http://google.com");
 }
 
+	@Test
+	public void check2() throws IOException{	
+		System.out.println("Hello");
+		Reporter.log("Inside 1st Methos"); 
+		
+	}
 }
